df03b21752735cf218332b29f392893eeef72d8c
Gaju Sandra <gajusandra872@gmail.com>
2026-07-07 17:26:25 +0200

    exactly from the picture in the exercise

--- part 1/part1-work/src/App.jsx
+++ part 1/part1-work/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(6)
+  const [neutral, setNeutral] = useState(2)
+  const [bad, setBad] = useState(1)
 
   return (
     <div>
