d15bd526dd1de49bac2c69b191f68cbaf0aba3e3
Gaju Sandra <gajusandra872@gmail.com>
2026-07-08 11:57:35 +0200

    the exercise 2.2 (adding the total to the previous)

--- part 2/src/App.jsx
+++ part 2/src/App.jsx
@@ -2,9 +2,14 @@ const Header = ({ name }) => <h1>{name}</h1>
 
 const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
+const Total = ({ parts }) => (
+  <p><strong>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong></p>
+)
+
 const Content = ({ parts }) => (
   <div>
     {parts.map(part => <Part key={part.id} part={part} />)}
+    <Total parts={parts} />
   </div>
 )
 
@@ -22,7 +27,8 @@ const App = () => {
     parts: [
       { name: 'Fundamentals of React', exercises: 10, id: 1 },
       { name: 'Using props to pass data', exercises: 7, id: 2 },
-      { name: 'State of a component', exercises: 14, id: 3 }
+      { name: 'State of a component', exercises: 14, id: 3 },
+      { name: 'Redux', exercises: 20, id: 4 }
     ]
   }
 
