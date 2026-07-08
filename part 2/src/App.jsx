05a024cd9e5bbf893abb967ea166208daa81f87c
Gaju Sandra <gajusandra872@gmail.com>
2026-07-08 16:11:37 +0200

    exercise 2.10 install of some packages and the db.json

--- part 2/src/App.jsx
+++ part 2/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ search, onChange }) => (
   <div>filter shown with: <input value={search} onChange={onChange} /></div>
@@ -23,12 +24,11 @@ const Persons = ({ persons, search }) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(res => setPersons(res.data))
+  }, [])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
