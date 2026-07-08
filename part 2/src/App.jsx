6347c23f3c00149b6d252b6ff4a7c9f53e1b3d1c
Gaju Sandra <gajusandra872@gmail.com>
2026-07-08 15:27:12 +0200

    exercise 2.7

--- part 2/src/App.jsx
+++ part 2/src/App.jsx
@@ -11,6 +11,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={(e) => {
         e.preventDefault()
+        if (persons.some(p => p.name === newName)) {
+          alert(`${newName} is already added to phonebook`)
+          return
+        }
         setPersons(persons.concat({ name: newName }))
         setNewName('')
       }}>
