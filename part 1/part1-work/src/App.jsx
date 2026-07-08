49f36a748ba392f88c7f1607929c46de27062a7a
Gaju Sandra <gajusandra872@gmail.com>
2026-07-08 10:44:12 +0200

    the anecdote vote

--- part 1/part1-work/src/App.jsx
+++ part 1/part1-work/src/App.jsx
@@ -11,12 +11,26 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  const randomAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const addVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={addVote}>vote</button>
+      <button onClick={randomAnecdote}>next anecdote</button>
     </div>
   )
 }
