ca0c38012825d9e7a2a79b6142fe3b2fb337e208
Gaju Sandra <gajusandra872@gmail.com>
2026-07-08 11:48:33 +0200

    exercise 2.1

--- part 2/src/App.jsx
+++ part 2/src/App.jsx
@@ -1,16 +1,32 @@
-const App = (props) => {
-  const { notes } = props
+const Header = ({ name }) => <h1>{name}</h1>
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul>
-    </div>
-  )
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => <Part key={part.id} part={part} />)}
+  </div>
+)
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+  </div>
+)
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10, id: 1 },
+      { name: 'Using props to pass data', exercises: 7, id: 2 },
+      { name: 'State of a component', exercises: 14, id: 3 }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
