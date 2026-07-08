9b62f4aecf0f146737219b0a44e46dc22ef1ac11
Gaju Sandra <gajusandra872@gmail.com>
2026-07-08 14:33:53 +0200

    exercise 2.4

--- part 2/src/App.jsx
+++ part 2/src/App.jsx
@@ -1,38 +1,54 @@
-const Header = ({ name }) => <h1>{name}</h1>
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Total = ({ parts }) => (
-  <p><strong>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong></p>
-)
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-    <Total parts={parts} />
-  </div>
-)
-
-const Course = ({ course }) => (
-  <div>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-  </div>
-)
-
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      { name: 'Fundamentals of React', exercises: 10, id: 1 },
-      { name: 'Using props to pass data', exercises: 7, id: 2 },
-      { name: 'State of a component', exercises: 14, id: 3 },
-      { name: 'Redux', exercises: 20, id: 4 }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
-}//same with 2.3
+  return (
+    <div>
+      // ...
+    </div>
+  )
+}
 
 export default App
