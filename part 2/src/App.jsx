ca56a937bc5d785c37c5c1f28c25d08183f08c1a
Gaju Sandra <gajusandra872@gmail.com>
2026-07-08 15:37:45 +0200

    exercise 2.7 step 2

--- part 2/src/App.jsx
+++ part 2/src/App.jsx
@@ -20,6 +20,11 @@ const App = () => {
       }}>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+      
+        </div>
+        <div>
+          number : <input />
+      
         </div>
         <div>
           <button type="submit">add</button>
