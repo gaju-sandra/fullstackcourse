0e70cc71d522e721d156847a37ced215a9aa4677
Gaju Sandra <gajusandra872@gmail.com>
2026-07-08 10:50:12 +0200

    the anecdote vote with the most votes and the anecdote of the day

--- part 1/part1-work/src/App.jsx
+++ part 1/part1-work/src/App.jsx
@@ -25,12 +25,19 @@ const App = () => {
     setVotes(copy)
   }
 
+  const mostVoted = votes.indexOf(Math.max(...votes))
+
   return (
     <div>
+      <h2>anecdote of the day</h2>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={addVote}>vote</button>
       <button onClick={randomAnecdote}>next anecdote</button>
+
+      <h2>anecdote with most votes</h2>
+      <p>{anecdotes[mostVoted]}</p>
+      <p>has {votes[mostVoted]} votes</p>
     </div>
   )
 }
