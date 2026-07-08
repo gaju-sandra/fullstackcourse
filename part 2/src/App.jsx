4a18c1810c271841d2ccaf2e3ddb57aa85452a29
Gaju Sandra <gajusandra872@gmail.com>
2026-07-08 15:13:08 +0200

    exercise 2.6

--- part 2/src/App.jsx
+++ part 2/src/App.jsx
@@ -1,50 +1,24 @@
-const Header = ({ name }) => <h1>{name}</h1>
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Total = ({ parts }) => (
-  <p><strong>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong></p>
-)
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-    <Total parts={parts} />
-  </div>
-)
-
-const Course = ({ course }) => (
-  <div>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-  </div>
-)
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        { name: 'Fundamentals of React', exercises: 10, id: 1 },
-        { name: 'Using props to pass data', exercises: 7, id: 2 },
-        { name: 'State of a component', exercises: 14, id: 3 },
-        { name: 'Redux', exercises: 11, id: 4 }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        { name: 'Routing', exercises: 3, id: 1 },
-        { name: 'Middlewares', exercises: 7, id: 2 }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
   return (
     <div>
-      {courses.map(course => <Course key={course.id} course={course} />)}
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
     </div>
   )
 }
